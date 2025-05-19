a1857c8c6723e292fbbb946788439248b954c6a6
BENHIMA-Mohamed-Amine <benhima.mohamed.amine@gmail.com>
2025-05-19 11:39:55 +0100

    Tester et terminer la partie backend

--- src/main/java/ma/enset/examenjee/benhima/ExamenJeeApplication.java
+++ src/main/java/ma/enset/examenjee/benhima/ExamenJeeApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @SpringBootApplication
 public class ExamenJeeApplication {
